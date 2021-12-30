0e7699af66e989658eeacbb383c445a45ced57e2
mz121 <mz.developer121@gmail.com>
2021-12-30 11:18:08 +0330

    added DependencyInjection class for Infrustracture layer

---
+++ InstagramBot.Infrustracture/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramBot.Core.Interfaces;
+using InstagramBot.Infrustracture.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InstagramBot.Infrustracture
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrustracture(this IServiceCollection services)
+        {
+            services.AddSingleton<IInstagramService, InstagramService>();
+            return services;
+        }
+    }
+}
