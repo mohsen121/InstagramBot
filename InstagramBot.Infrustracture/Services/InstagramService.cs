09c56cc76b2fa908b05318d19dd074c543f547eb
mz121 <mz.developer121@gmail.com>
2021-12-30 11:15:46 +0330

    Added Infrustracture layer added InstagramService class

---
+++ InstagramBot.Infrustracture/Services/InstagramService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InstagramBot.Core.Interfaces;
+using InstagramBot.Core.ViewModels;
+
+namespace InstagramBot.Infrustracture.Services
+{
+    public class InstagramService : IInstagramService
+    {
+        public Task<string> CreatePost(string caption, string[] images)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<InstagramPostViewModel>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
